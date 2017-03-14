818e6771241d12aa0a54c7593922807809fe45dc
Kotaro Hashimoto <kotaro.hashimoto.jv@hitachi.com>
2017-03-14 13:45:46 +0900

    for windows and python3.6
    Signed-off-by: Kotaro Hashimoto <kotaro.hashimoto.jv@hitachi.com>

--- bitarb.py
+++ bitarb.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
-from urllib import urlopen
+
+from urllib.request import urlopen
 from json import load
 from datetime import datetime
 import oandapy
 
-from  Tkinter import *
+from tkinter import *
 
 
 class Window:
@@ -13,11 +14,6 @@ class Window:
     def __init__(self, title):
         self.root = Tk()
         self.root.title(title)
-#        root.geometry('400x300')
-
-#        self.editbox = Entry(width = 3)
-#        self.editbox.insert(END, '1')
-#        self.editbox.pack()
 
         self.str = StringVar()
         self.str.set('')
@@ -28,12 +24,11 @@ class Window:
 
     def update(self):
         self.str.set('Last Update:  ' + datetime.now().strftime('%Y/%m/%d %H:%M:%S'))
-        
+
 
 class OANDA:
 
-    usdjpy = 0
-    eurjpy = 0
+    PRICE = {'USD_JPY':0, 'EUR_JPY':0, 'GBP_JPY':0}
     
     def __init__(self, root):
 
@@ -42,29 +37,31 @@ class OANDA:
 
         self.usdjpy = StringVar()
         self.usdjpy.set('')
-        self.label = Label(root, textvariable = self.usdjpy)
-        self.label.pack()
+        Label(root, textvariable = self.usdjpy).pack()
 
         self.eurjpy = StringVar()
         self.eurjpy.set('')
-        self.label = Label(root, textvariable = self.eurjpy)
-        self.label.pack()
+        Label(root, textvariable = self.eurjpy).pack()
+
+        self.gbpjpy = StringVar()
+        self.gbpjpy.set('')
+        Label(root, textvariable = self.gbpjpy).pack()
 
         self.oanda = oandapy.API(environment='practice', access_token='f80296b600eddebbb0402eeabce34139-55d481314b19c1127978ecd05c9dca65')
 
+    def update(self, lstr, symbol):
+
+        prices = self.oanda.get_prices(instruments = symbol).get('prices')
+        OANDA.PRICE[symbol] = (prices[0].get('ask') + prices[0].get('bid')) / 2.0
+        ask = str(int(1000 * prices[0].get('ask')))
+        bid = str(int(1000 * prices[0].get('bid')))
+        lstr.set(symbol.replace('_', '') + '\t\t\t' + ask[:3] + '.' + ask[3:] + '\t' + bid[:3] + '.' + bid[3:])
+
     def getPrice(self):
 
-        prices = self.oanda.get_prices(instruments='USD_JPY').get('prices')
-        OANDA.usdjpy = (prices[0].get('ask') + prices[0].get('bid')) / 2.0
-        ask = str(int(1000 * prices[0].get('ask')))
-        bid = str(int(1000 * prices[0].get('bid')))
-        self.usdjpy.set('USDJPY\t\t\t' + ask[:3] + '.' + ask[3:] + '\t' + bid[:3] + '.' + bid[3:])
-
-        prices = self.oanda.get_prices(instruments='EUR_JPY').get('prices')
-        OANDA.eurjpy = (prices[0].get('ask') + prices[0].get('bid')) / 2.0
-        ask = str(int(1000 * prices[0].get('ask')))
-        bid = str(int(1000 * prices[0].get('bid')))
-        self.eurjpy.set('EURJPY\t\t\t' + ask[:3] + '.' + ask[3:] + '\t' + bid[:3] + '.' + bid[3:])
+        self.update(self.usdjpy, 'USD_JPY')
+        self.update(self.eurjpy, 'EUR_JPY')
+        self.update(self.gbpjpy, 'GBP_JPY')
 
 
 class Exchange:
@@ -83,17 +80,22 @@ class Exchange:
 
         self.ask = 0
         self.bid = 0
+        self.p = 0
 
     def getPrice(self):
 
         data = load(urlopen(self.url))
-    #    print(exchange + '\t' + str(data[last]) + '\t' +  str(data[ask]) + '\t' +  str(data[bid]))
+
         self.ask = int(data[self.sask])
         self.bid = int(data[self.sbid])
-
+        
+        up = int(data[self.last])
+#        self.label.configure(fg = ('green' if self.p < up else 'red'))
+        self.p = up
+        
         a = str(self.ask)
         b = str(self.bid)
-        l = str(int(data[self.last]))
+        l = str(self.p)
     
         self.str.set(self.name + (' ' * (20 - len(self.name))) + '\t' + l[:3] + ',' + l[3:] + '\t' +  a[:3] + ',' + a[3:] + '\t' +  b[:3] + ',' + b[3:])
 
@@ -104,15 +106,22 @@ class USDExchange(Exchange):
         Exchange.__init__(self, root, name, url, last, sask, sbid)
 
     def getPrice(self):
-
+    
         data = load(urlopen(self.url))
-    #    print(exchange + '\t' + str(data[last]) + '\t' +  str(data[ask]) + '\t' +  str(data[bid]))
-        self.ask = float(data[self.sask]) * OANDA.usdjpy
-        self.bid = float(data[self.sbid]) * OANDA.usdjpy
+
+        if self.name == 'BTC-e':
+            data = data['btc_usd']
+
+        self.ask = float(data[self.sask]) * OANDA.PRICE['USD_JPY']
+        self.bid = float(data[self.sbid]) * OANDA.PRICE['USD_JPY']
+        
+        up = float(data[self.last]) * OANDA.PRICE['USD_JPY']
+#        self.label.configure(fg = ('green' if self.p < up else 'red'))
+        self.p = up
 
         a = str(int(round(self.ask)))
         b = str(int(round(self.bid)))
-        l = str(int(round(float(data[self.last]) * OANDA.usdjpy)))
+        l = str(int(round(self.p)))
 
         self.str.set(self.name + (' ' * (20 - len(self.name))) + '\t' + l[:3] + ',' + l[3:] + '\t' +  a[:3] + ',' + a[3:] + '\t' +  b[:3] + ',' + b[3:])
 
@@ -127,31 +136,19 @@ Exchange(window.root, 'Zaif', 'https://api.zaif.jp/api/1/ticker/btc_jpy', 'last'
 Exchange(window.root, 'coincheck', 'https://coincheck.com/api/ticker', 'last', 'ask', 'bid'), \
 #Exchange(window.root, 'Quoine', 'https://api.quoine.com/products/5', 'last_traded_proce', 'market_ask', 'market_bid'), \
 USDExchange(window.root, 'Bitstamp', 'https://www.bitstamp.net/api/v2/ticker/btcusd/', 'last', 'ask', 'bid'), \
+USDExchange(window.root, 'BTC-e', 'https://btc-e.com/api/3/ticker/btc_usd', 'last', 'buy', 'sell'), \
 OANDA(window.root), \
 ]
 exchangeList.reverse()
-
+exchangeList = tuple(exchangeList)
 
 def updatePrice():
-
-    minAsk = 100000000
-    maxBid = 0
 
     for e in exchangeList:
         e.getPrice()
 
-#        if e.ask < minAsk:
-#            minAsk = e.ask
-#            askLabel = e.label
-#        if maxBid < e.bid:
-#            maxBid = e.bid
-#            bidLabel = e.label
-
-#    askLabel.configure(fg = 'red')
-#    bidLabel.configure(fg = 'blue')
-
     window.update()    
-    window.root.after(1000, updatePrice)
+    window.root.after(5000, updatePrice)
 
 updatePrice()
 window.root.mainloop()
