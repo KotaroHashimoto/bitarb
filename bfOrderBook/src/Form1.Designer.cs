baddf1d559694f33ac596d7aa5b96f031e183204
Kotaro Hashimoto <hasimoto.kotaro@gmail.com>
2017-08-30 20:40:10 +0900

    bf order book
    Signed-off-by: Kotaro Hashimoto <hasimoto.kotaro@gmail.com>

---
+++ bfOrderBook/src/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace bfOrderBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.listBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(390, 604);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseClick);
+            this.listBox1.MouseCaptureChanged += new System.EventHandler(this.listBox1_MouseCaptureChanged);
+            this.listBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDown);
+            this.listBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseUp);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(277, 610);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 19);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(209, 612);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Jump to:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 638);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "BitFlyer FX OrderBook";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ bfOrderBook/src/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using BitflyerApi;
+using System.Net;
+
+namespace bfOrderBook
+{
+    public partial class Form1 : Form
+    {
+
+        BitflyerClient client;
+        bool mask = false;
+        int SMAX = 0;
+
+        String[] ss = new string[] {
+                    "",
+                    " ",
+                    "  ",
+                    "   ",
+                    "    ",
+                    "     ",
+                    "      ",
+                    "       ",
+                    "        ",
+                    "         "};
+
+        String s19 = new string(' ', 19);
+        String s46 = new string(' ', 46);
+        String sc = ":";
+
+        public Form1()
+        {
+            InitializeComponent();            
+
+            client = new BitflyerClient("", "", ProductCode.FX_BTC_JPY);
+        }
+
+        async Task getOrderBook()
+        {
+
+            List<string> orderBookA = null;
+            List<string> orderBookB = null;
+            List<string> orderBook = null;
+            Board board;
+
+            bool first = true;
+
+            listBox1.Items.Add("Connecting to BitFlyer server ...");
+
+
+            while (true)
+            {
+                if (!first)
+                {
+                    if (listBox1.DataSource.Equals(orderBookA))
+                    {
+                        orderBook = orderBookB;
+                    }
+                    else
+                    {
+                        orderBook = orderBookA;
+                    }
+                    orderBook.Clear();
+                }
+
+                try
+                {
+                    board = await client.GetBoard();
+                }
+                catch (Exception ex)
+                {
+                    listBox1.DataSource = null;
+                    listBox1.Items.Add("Connecting to BitFlyer server ...");
+                    listBox1.Items.Add(ex);
+
+                    first = true;
+
+                    await Task.Delay(1000);
+                    continue;
+                }
+
+                if (first)
+                {
+                    SMAX = Math.Max(board.Asks.Count, board.Bids.Count) + 50;
+
+                    orderBookA = new List<string>(SMAX * 2 + 1);
+                    orderBookB = new List<string>(SMAX * 2 + 1);
+                    orderBook = orderBookA;
+                }
+
+
+                int n = SMAX - board.Asks.Count;
+                for (int i = 0; i < n; i++)
+                {
+                    String index = (SMAX - i).ToString();
+                    orderBook.Add(ss[4 - index.Length] + index + sc);
+                }
+
+                for (int i = (0 < n ? board.Asks.Count : SMAX) - 1; 0 <= i; i--)
+                {
+                    String index = (i + 1).ToString();
+                    String[] ask = board.Asks[i].Size.ToString().Split('.');
+                    String pr = board.Asks[i].Price.ToString();
+
+                    orderBook.Add(ss[4 - index.Length] + index + sc +
+                        ss[5 - ask[0].Length] + ask[0] + (ask.Length == 2 ? ("." + ask[1] + ss[8 - ask[1].Length]) : ss[9]) +
+                        ss[9 - pr.Length] + pr);
+                }
+
+                String p = board.MiddlePrice.ToString();
+                orderBook.Add(s19 + ss[9 - p.Length] + p);
+
+                n = SMAX - board.Bids.Count;
+                for (int i = 0; i < (0 < n ? board.Bids.Count : SMAX); i++)
+                {
+                    String index = (i + 1).ToString();
+                    String[] bid = board.Bids[i].Size.ToString().Split('.');
+                    String pr = board.Bids[i].Price.ToString();
+
+                    orderBook.Add(s19 +
+                        ss[9 - pr.Length] + pr + ss[2] +
+                        ss[5 - bid[0].Length] + bid[0] + (bid.Length == 2 ? ("." + bid[1] + ss[8 - bid[1].Length]) : ss[9]) +
+                        ss[2] + sc + ss[4 - index.Length] + index);
+                }
+
+                for (int i = 0; i < n; i++)
+                {
+                    String index = (board.Bids.Count + i + 1).ToString();
+                    orderBook.Add(s46 + sc + ss[4 - index.Length] + index);
+                }
+
+
+                int foc = listBox1.SelectedIndex;
+                int pos = listBox1.TopIndex;
+
+                if (!mask)
+                {
+                    listBox1.DataSource = orderBook;
+                }
+
+                listBox1.SelectedIndex = foc;
+                listBox1.TopIndex = pos;
+
+                if (first)
+                {
+                    listBox1.TopIndex = SMAX - 20;
+                    first = false;
+                }
+
+                await Task.Delay(1000);
+            }
+        }
+
+
+        private void listBox1_MouseCaptureChanged(object sender, EventArgs e)
+        {
+            mask = !mask;
+        }
+
+        private void listBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            mask = false;
+        }
+
+        private void listBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mask = false;
+        }
+
+        private void listBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mask = false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            if (Int32.TryParse(textBox1.Text, out int numValue))
+            {
+                if(0 < numValue && numValue < SMAX)
+                {
+                    listBox1.SelectedIndex = SMAX - numValue;
+                    listBox1.TopIndex = listBox1.SelectedIndex - 20;
+                }
+                else if (-1 * SMAX < numValue && numValue < 0)
+                {
+                    listBox1.SelectedIndex = SMAX + (-1 * numValue);
+                    listBox1.TopIndex = listBox1.SelectedIndex - 20;
+                }
+                else if(numValue == 0) {
+                    listBox1.SelectedIndex = SMAX;
+                    listBox1.TopIndex = listBox1.SelectedIndex - 20;
+                }
+            }
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            await getOrderBook();
+        }
+    }
+}
