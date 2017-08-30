b39ee7187d8cc522a74af43c3186a06b54ccd9f5
Kotaro Hashimoto <hasimoto.kotaro@gmail.com>
2017-08-31 02:05:14 +0900

    v3

--- bfOrderBook/src/Form1.Designer.cs
+++ bfOrderBook/src/Form1.Designer.cs
@@ -70,6 +70,7 @@
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(16, 610);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(172, 23);
