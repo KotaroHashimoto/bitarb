db9384b9e8c9c7d2327878963d85fd63b4f068fe
Kotaro Hashimoto <hasimoto.kotaro@gmail.com>
2017-08-30 23:26:12 +0900

    cenceller
    Signed-off-by: Kotaro Hashimoto <hasimoto.kotaro@gmail.com>

---
+++ bfCanceller/src/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace bfCanceller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(536, 214);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(3, 219);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(530, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Cancell All Open Orders";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "BitFlyer FX Order Canceller";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ bfCanceller/src/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using BitflyerApi;
+using System.Net;
+
+namespace bfCanceller
+{
+
+    public partial class Form1 : Form
+    {
+
+        String[] ss = new string[] {
+                    "",
+                    " ",
+                    "  ",
+                    "   ",
+                    "    ",
+                    "     ",
+                    "      ",
+                    "       ",
+                    "        ",
+                    "         "};
+
+        bool mask = false;
+        bool processing = false;
+        BitflyerClient client = null;
+
+        List<Order> orders = null;
+
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        async Task getOpenOrders()
+        {
+
+            List<string> orderListA = new List<string>();
+            List<string> orderListB = new List<string>();
+            List<string> orderList = orderListA;
+
+            listBox1.Items.Add("Connecting to BitFlyer server ...");
+
+
+            while (true)
+            {
+                if (listBox1.DataSource != null)
+                {
+                    if (listBox1.DataSource.Equals(orderListA))
+                    {
+                        orderList = orderListB;
+                    }
+                    else
+                    {
+                        orderList = orderListA;
+                    }
+                }
+
+                if(orderList != null)
+                {
+                    orderList.Clear();
+                }
+
+                try
+                {
+                    orders = await client.GetMyActiveOrders();
+                }
+                catch (Exception ex)
+                {
+                    listBox1.DataSource = null;
+                    listBox1.Items.Add(ex);
+                    listBox1.Items.Add("Connecting to BitFlyer server ...");
+
+                    await Task.Delay(1000);
+                    continue;
+                }
+
+                if (orders == null)
+                {
+                    orderList.Add("No Open Order.");
+                }
+                else if(orders.Count == 0)
+                {
+                    orderList.Add("No Open Order.");
+                }
+                else
+                {
+                    foreach (Order oo in orders)
+                    {
+                        string side = oo.Side.ToString();
+                        string[] amount = oo.OutstandingSize.ToString().Split('.');
+                        string price = oo.Price.ToString();
+                        string date = oo.Date.ToLongTimeString() + ss[1] + oo.Date.ToLongDateString();
+
+                        orderList.Add(ss[5 - side.Length] + side + ss[5 - amount[0].Length] + amount[0].Length +
+                            (amount.Length == 2 ? ("." + amount[1] + ss[8 - amount[1].Length]) : ss[9]) + " BTC at " +
+                            ss[7 - price.Length] + price + ss[3] + date);
+                    }
+                }
+
+                int foc = listBox1.SelectedIndex;
+                int pos = listBox1.TopIndex;
+
+                if (!mask)
+                {
+                    listBox1.DataSource = orderList;
+                }
+
+                listBox1.SelectedIndex = (foc < orders.Count) ? foc : 0;
+                listBox1.TopIndex = (pos < orders.Count) ? pos : 0;
+
+                await Task.Delay(1000);
+            }
+        }
+
+
+        private void listBox1_MouseCaptureChanged(object sender, EventArgs e)
+        {
+            mask = !mask;
+        }
+
+        private void listBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            mask = false;
+        }
+
+        private void listBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mask = false;
+        }
+
+        private void listBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mask = false;
+        }
+
+        private async void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+            if (!processing && orders != null)
+            {
+                processing = true;
+
+                try
+                {
+                    if (listBox1.SelectedIndex < orders.Count) {
+                        await client.CancelOrder(orders.ElementAt(listBox1.SelectedIndex));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    listBox1.DataSource = null;
+                    listBox1.Items.Add(ex);
+                    listBox1.Items.Add("Failed to cancel order.");
+
+                    await Task.Delay(1000);
+                }
+
+                processing = false;
+            }
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+
+            String line = null;
+            String key = null;
+            String secret = null;
+
+            try { 
+                System.IO.StreamReader file = new System.IO.StreamReader("C:\\ProgramData\\apikey.txt");
+
+                while ((line = file.ReadLine()) != null)
+                {
+                    if (key == null)
+                    {
+                        key = line;
+                    }
+                    else if (secret == null)
+                    {
+                        secret = line;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                file.Close();
+            }
+            catch(Exception ex)
+            {
+                listBox1.Items.Add(ex);
+            }
+
+            if (key != null && secret != null)
+            {
+                client = new BitflyerClient(key, secret, ProductCode.FX_BTC_JPY);
+                await getOpenOrders();
+            }
+            else
+            {
+                listBox1.Items.Add("Failed to read API key.");
+            }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await client.CancelAllOrders();
+        }
+    }
+}
